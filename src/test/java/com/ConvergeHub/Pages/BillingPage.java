47f9ad0c1a666bbfe3530b5a3b4a4e693fd21504
Arpita <Arpita@sunit-PC>
2019-02-14 12:27:57 +0530

    Commit-Add Invoice

--- src/test/java/com/ConvergeHub/Pages/BillingPage.java
+++ src/test/java/com/ConvergeHub/Pages/BillingPage.java
@@ -18,6 +18,55 @@ public class BillingPage extends Base
 	@FindBy(id="usage_unit_select_drp_dwn") public WebElement Unit;//Select
 	@FindBy(id="stock_no") public WebElement productStock;//Edit
 	@FindBy(xpath="//button[@id='productValidation' and @type='submit']") public WebElement btnproductSave;//Button
+
+	
+	
+	//Quotes-Elements
+	@FindBy(id="account_name_field") public WebElement accountName;//Edit
+	@FindBy(id="valid_till") public WebElement validTill;//Edit
+	@FindBy(id="terms_conditions") public WebElement TermsandCondition;//Edit
+	@FindBy(id="description") public WebElement QuotesDescription;//Edit
+	@FindBy(id="bean_id_1_name") public WebElement QuotesProduct;//Edit----After Selecting Product from TestData Click on Qty Field
+	@FindBy(id="quantity_1") public WebElement Quanitity;//Edit
+	@FindBy(id="list_price_1") public WebElement Rate;//Edit
+	@FindBy(id="currency_id_select_drp_dwn") public WebElement Currency;//Select
+	@FindBy(xpath="//button[@id='accountValidation'and @type='submit']") public WebElement btnSave;//Button
+	@FindBy(xpath="//div[@class='ui-datepicker-title']//select[@class='ui-datepicker-month']") public WebElement calMonth;//Select
+	@FindBy(xpath="//div[@class='ui-datepicker-title']//select[@class='ui-datepicker-year']") public WebElement calYear;//Select
+	
+	
+	/*
+	//-------------For Selecting the Day
+	List<WebElement> allDates=driver.findElements(By.xpath("//table[@class='ui-datepicker-calendar']//td"));
+	
+	for(WebElement ele:allDates)
+	{
+		
+		String date=ele.getText();
+		
+		if(date.equalsIgnoreCase("28"))
+		{
+			ele.click();
+			break;
+		}
+		
+	}
+	//----------For Selecting the Month
+	//div[@class='ui-datepicker-title']//select[@class='ui-datepicker-month']
+	
+	//----------For Selecting the Year 
+	//div[@class='ui-datepicker-title']//select[@class='ui-datepicker-year']
+	
+	*/
+	
+	//Invoice-Elements
+	@FindBy(id="invoice_date") public WebElement invoiceDate;//DatePicker
+	@FindBy(id="due_date") public WebElement dueDate;//DatePicker
+	@FindBy(id="status_select_drp_dwn") public WebElement invStatus ;//Select
+	
+	
+	
+	//Payment-Elements
 	
 	
 	
--- src/test/java/com/ConvergeHub/TestCases/TC_Billing.java
+++ src/test/java/com/ConvergeHub/TestCases/TC_Billing.java
@@ -28,7 +28,8 @@ import com.ConvergeHub.Utilities.TestUtil;
 
 
 public class TC_Billing extends Base  
-{    
+{  
+	
 	@Test(priority=0,groups={"Regression"},description="Create a New Product from Billing Module")
 	
 	public static void AddProduct() throws InterruptedException
@@ -142,9 +143,342 @@ public class TC_Billing extends Base
 	    
 	    ////Validation that Product edited successfully
 	    Assert.assertTrue(driver.findElement(By.xpath("//span[@id='header_notification_msg']")).getText().contains("Updated"));
-	 
-	    
+	 	    
 	}
 	
+	@Test(priority=2,groups={"Regression"},description="Create a New Quote from Billing Module")
+	
+	public static void CreateQuote() throws InterruptedException
+	{
+		//Initialize the Page Class
+		DealPage deal=new DealPage();
+		CasePage casepg=new CasePage();
+		ActivitiesPage activities=new ActivitiesPage();
+		BillingPage billing=new BillingPage();
+	
+	/*-------------------------Login Code
+		LoginPage login=new LoginPage();
+		login.username.clear();
+	    login.username.sendKeys(config.getProperty("UserName"));
+		login.password.sendKeys(config.getProperty("Password"));
+	    login.login.click();
+	    System.out.println("Successfully Logged");
+	    wait=new WebDriverWait(driver,30); 
+	    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'My Dashboard')]")));	
+	    driver.get("https://staging.convergehub.com/targets/add");
+	    -------------------------------------------*/
+	    
+		//Redirecting to the Add Quotation Page
+		driver.get("https://"+config.getProperty("Environment")+".convergehub.com/quotations/add/");
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+	    
+	    //===========Filled up the Add Quotation Screen=====================
+		
+		//Select the Account Name
+	    billing.accountName.click();
+	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	    String acctNameQuote=excel.getCellDataUpd("Billing","AccountName", 1);
+	    List<WebElement> name_List= driver.findElements(By.xpath("//div[@id='alls_tab_account_name']/ul/li"));
+	    //List<WebElement> name_List=acctname.findElements(By.tagName("li"));
+	    for (WebElement li : name_List) 
+	    {
+	    if (li.getText().equals(acctNameQuote)) 
+	    {
+	    	 System.out.println(li.getText());
+	         li.click();
+	         break;
+	       }
+	    }
+	    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		
+		//------Select the Date from the Valid Till Date Picker	
+	    String Day=excel.getCellDataUpd("Billing","Day", 1);
+	    String Month=excel.getCellDataUpd("Billing","Month", 1);
+	    String Year=excel.getCellDataUpd("Billing","Year", 1);
+	    
+	    billing.validTill.click();
+	    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	    new Select(billing.calMonth).selectByVisibleText(Month);
+	    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	    new Select(billing.calYear).selectByVisibleText(Year);
+	    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	    
+	  //-------------For Selecting the Day
+		List<WebElement> allDates=driver.findElements(By.xpath("//table[@class='ui-datepicker-calendar']//td"));
+		
+		for(WebElement ele:allDates)
+		{
+			
+			String date=ele.getText();
+			
+			if(date.equalsIgnoreCase(Day))
+			{
+				ele.click();
+				break;
+			}
+			
+		}
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);	
+		//------Enter the Terms & Conditions
+	    String TermsCondition=excel.getCellDataUpd("Billing","TermsCondition", 1);
+	    billing.TermsandCondition.sendKeys(TermsCondition);
+	    
+		//------Enter the Quotes Description
+	    String QuotesDescription=excel.getCellDataUpd("Billing","QuotesDescription", 1);
+	    billing.QuotesDescription.sendKeys(QuotesDescription);
+	    
+        //Click the Details tab
+	    driver.findElement(By.linkText("Details")).click();
+	    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	    
+	    //Enter the Product/Service
+	    String productService=excel.getCellDataUpd("Billing","productService", 1);
+	    billing.QuotesProduct.sendKeys(productService);
+	    	    
+	    //Enter the Quantity
+	    billing.Quanitity.click();
+	    String Qunatity=excel.getCellDataUpd("Billing","Qty", 1);
+	    billing.Quanitity.sendKeys(Qunatity);
+	    
+	    //Enter the Rate
+	    billing.Rate.click();
+	    String Rate=excel.getCellDataUpd("Billing","Rate", 1);
+	    billing.Rate.sendKeys(Rate);
+	    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	    
+	    //Select the Currency
+	    String Currency=excel.getCellDataUpd("Billing","Currency", 1);
+	    new Select(billing.Currency).selectByValue(Currency);
+	    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	    
+
+	    //Click-Save Quotation Button
+	    billing.btnSave.click();
+	    
+	    
+	    //Assertion statement added for the verification
+	    WebDriverWait waittasksave = new WebDriverWait (driver, 20);
+	    waittasksave.until(ExpectedConditions.visibilityOf(deal.SuccessNotificationMsg));
+	    
+	    Assert.assertTrue(deal.SuccessNotificationMsg.getText().toString().contains("Created"));
+	    
+	  	String  baseurl=driver.getCurrentUrl();
+		String arr[]=baseurl.split("/");		
+		String Quote_ID=arr[arr.length-1];
+		TestUtil.writeProperty("Quote_ID", Quote_ID);  
+	    	
+	}
+	
+@Test(priority = 3,groups={"Regression"},description="Edit a Quote")
+
+	public static void editQuote() throws InterruptedException
+	{
+		LeadPage lead=new LeadPage();
+		CollaborationPage clb=new CollaborationPage();
+		BillingPage billing=new BillingPage();
+		
+		/*-------------------------Login Code
+		LoginPage login=new LoginPage();		
+		login.username.clear();
+	    login.username.sendKeys(config.getProperty("UserName"));
+		login.password.sendKeys(config.getProperty("Password"));
+	    login.login.click();
+	    System.out.println("Successfully Logged");
+	    wait=new WebDriverWait(driver,20); 
+	    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'My Dashboard')]")));
+	     -------------------------------------------*/
+
+	    driver.get("https://"+config.getProperty("Environment")+".convergehub.com/quotations/add/"+SavedData.getProperty("Quote_ID"));
+	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	    
+	    //Edit  the Product Quantity & rate
+	    
+        //Click the Details tab
+	    driver.findElement(By.linkText("Details")).click();
+	    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	    
+   	    
+	    //Enter the Quantity
+	    billing.Quanitity.click();
+	    billing.Quanitity.clear();
+	    String Qunatitynew=excel.getCellDataUpd("Billing","QtyNew", 1);
+	    billing.Quanitity.sendKeys(Qunatitynew);
+	    
+	    //Enter the Rate
+	    billing.Rate.click();
+	    billing.Rate.clear();
+	    String RateNew=excel.getCellDataUpd("Billing","RateNew", 1);
+	    billing.Rate.sendKeys(RateNew);
+	    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	
+	    //Click-Save Quotation Button
+	    billing.btnSave.click();
+	    
+	    WebDriverWait wait = new WebDriverWait (driver, 20);
+	    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@id='header_notification_msg']")));
+	    
+	    ////Validation that Product edited successfully
+	    Assert.assertTrue(driver.findElement(By.xpath("//span[@id='header_notification_msg']")).getText().contains("Updated"));
+	 	    
+	}	
+	
+@Test(priority=4,groups={"Regression"},description="Create a New Invoice from Billing Module")
+
+public static void CreateInvoice() throws InterruptedException
+{
+	//Initialize the Page Class
+	DealPage deal=new DealPage();
+	BillingPage billing=new BillingPage();
+
+/*-------------------------Login Code
+	LoginPage login=new LoginPage();
+	login.username.clear();
+    login.username.sendKeys(config.getProperty("UserName"));
+	login.password.sendKeys(config.getProperty("Password"));
+    login.login.click();
+    System.out.println("Successfully Logged");
+    wait=new WebDriverWait(driver,30); 
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'My Dashboard')]")));	
+    driver.get("https://staging.convergehub.com/targets/add");
+    -------------------------------------------*/
+    
+	//Redirecting to the Add Invoice Page
+	driver.get("https://"+config.getProperty("Environment")+".convergehub.com/invoices/add/");
+	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	
+    
+    //===========Filled up the Add Invoice Screen=====================
+	
+	//Select the Account Name
+    billing.accountName.click();
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    String acctNameQuote=excel.getCellDataUpd("Billing","AccountName", 1);
+    List<WebElement> name_List= driver.findElements(By.xpath("//div[@id='alls_tab_account_name']/ul/li"));
+   
+    for (WebElement liinv : name_List) 
+    {
+    if (liinv.getText().equals(acctNameQuote)) 
+    {
+    	 System.out.println(liinv.getText());
+    	 liinv.click();
+    	 driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+         break;
+       }
+    }
+   
+	
+	//------Select the Date from the-Invoice Date Date Picker	
+    String invDay=excel.getCellDataUpd("Billing","InvDay", 1);
+    String invMonth=excel.getCellDataUpd("Billing","InvMonth", 1);
+    String invYear=excel.getCellDataUpd("Billing","InvYear", 1);
+    
+    billing.invoiceDate.click();
+    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    new Select(billing.calMonth).selectByVisibleText(invMonth);
+    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    new Select(billing.calYear).selectByVisibleText(invYear);
+    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    
+    	//-------------For Selecting the Day
+	List<WebElement> allDatesinv=driver.findElements(By.xpath("//table[@class='ui-datepicker-calendar']//td"));
+	
+	for(WebElement eleinv:allDatesinv)
+	{
+		
+		String dateinv=eleinv.getText();
+		System.out.println(dateinv);
+		if(dateinv.equalsIgnoreCase(invDay))
+		{
+			eleinv.click();
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);	
+			break;
+		}
+		
+	}
+	
+	//Select the Invoice Status
+	String invStatus=excel.getCellDataUpd("Billing","invStatus", 1);
+	new Select(billing.invStatus).selectByVisibleText(invStatus);
+	
+	
+	//------Select the Date from the-Due Date Date Picker	
+    String dueDay=excel.getCellDataUpd("Billing","dueDay", 1);
+    String dueMonth=excel.getCellDataUpd("Billing","dueMonth", 1);
+    String dueYear=excel.getCellDataUpd("Billing","dueYear", 1);
+    
+    billing.dueDate.click();
+    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    new Select(billing.calMonth).selectByVisibleText(dueMonth);
+    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    new Select(billing.calYear).selectByVisibleText(dueYear);
+    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    
+    	//-------------For Selecting the Day
+	List<WebElement> allDatesdue=driver.findElements(By.xpath("//table[@class='ui-datepicker-calendar']//td"));
+	
+	for(WebElement eledue:allDatesdue)
+	{
+		
+		String datedue=eledue.getText();
+		
+		if(datedue.equalsIgnoreCase(dueDay))
+		{
+			eledue.click();
+			driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);	
+			break;
+		}
+		
+	}
+		
+	//------Enter the Terms & Conditions
+    String InvTermsCondition=excel.getCellDataUpd("Billing","InvTermsCondition", 1);
+    billing.TermsandCondition.sendKeys(InvTermsCondition);
+    
+	//------Enter the Description
+    String InvoiceDescription=excel.getCellDataUpd("Billing","InvDescription", 1);
+    billing.QuotesDescription.sendKeys(InvoiceDescription);
+    
+    //Click the Details tab
+    driver.findElement(By.linkText("Details")).click();
+    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    
+    //Enter the Product/Service
+    String productService=excel.getCellDataUpd("Billing","productService", 1);
+    billing.QuotesProduct.sendKeys(productService);
+    	    
+    //Enter the Quantity
+    billing.Quanitity.click();
+    String Qunatity=excel.getCellDataUpd("Billing","Qty", 1);
+    billing.Quanitity.sendKeys(Qunatity);
+    
+    //Enter the Rate
+    billing.Rate.click();
+    String Rate=excel.getCellDataUpd("Billing","Rate", 1);
+    billing.Rate.sendKeys(Rate);
+    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    
+    //Select the Currency
+    String Currency=excel.getCellDataUpd("Billing","Currency", 1);
+    new Select(billing.Currency).selectByValue(Currency);
+    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    
+
+    //Click-Save Button
+    billing.btnSave.click();
+    
+    
+    //Assertion statement added for the verification
+    WebDriverWait waittasksave = new WebDriverWait (driver, 20);
+    waittasksave.until(ExpectedConditions.visibilityOf(deal.SuccessNotificationMsg));
+    
+    Assert.assertTrue(deal.SuccessNotificationMsg.getText().toString().contains("Created"));
+    
+  	String  baseurl=driver.getCurrentUrl();
+	String arr[]=baseurl.split("/");		
+	String Invoice_ID=arr[arr.length-1];
+	TestUtil.writeProperty("Invoice_ID", Invoice_ID);  
+    	
+}	
 		
 }
